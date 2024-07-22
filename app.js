df9a2f8139a4f110359b457c1e5ba32d923e54e9
Cherar <lareugerardo@gmail.com>
2024-07-22 09:43:00 -0300

    chore: update repo

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const createError = require('http-errors');
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const cors = require('cors');
 
 const usersRouter = require('./routes/users');
 
@@ -12,7 +11,6 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(cors);
 
 app.use('/users', usersRouter);
 
